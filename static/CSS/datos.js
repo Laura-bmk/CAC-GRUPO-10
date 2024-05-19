0cc455194dd8c30d4414cd48b14e745cef0f749f
Laura-bmk <laura_bmk@hotmail.com>
2024-05-19 17:04:34 -0300

    ensamble el proyecto de Héctor y Giu y cambié estilos del Registro y Login para armonizar

---
+++ static/CSS/datos.js
@@ -0,0 +1,435 @@
+//Array de Fotos
+fotos=[
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    "https://rickandmortyapi.com/api/character/avatar/6.jpeg"
+]
+
+//Array de celulares
+let celulares= [
+    { 
+    "id": 1,
+    "modelo":"XIAOMI M5s",
+    "ram": "4gb",
+    "almacenamiento": "64gb",
+    "precio": 180000,
+    "image":"./static/img/imagenes/1.png",
+    "tendencias": "OFERTA DEL DIA"
+    },
+    { 
+    "id": 2,
+    "modelo":"HONOR X6s Dual SIM",
+    "ram": "4gb",
+    "almacenamiento": "128gb",
+    "precio": 160000,
+    "image":"./static/img/imagenes/2.png",
+    "tendencias": "Llega gratis mañana"
+    },
+    { 
+    "id": 3,
+    "modelo":"XIAOMI POCO F5 5G Dual SIM",
+    "ram": "12gb",
+    "almacenamiento": "256gb",
+    "precio": 562000,
+    "image":"./static/img/imagenes/3.png",
+    "tendencias": "Llega gratis mañana"
+    },
+    {
+    "id": 4,
+    "modelo":"XIAOMI POCO X6 Pro 5G Dual SIM",
+    "ram": "12gb",
+    "almacenamiento": "512gb",
+    "precio": 521000,
+    "image":"./static/img/imagenes/4.png" ,
+    "tendencias": "Envio Gratis"
+    },
+    {
+    "id": 5,
+    "modelo":"XIAOMI POCO X6 5G Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 424000,
+    "image":"./static/img/imagenes/5.png",
+    "tendencias": "Llega HOY"
+    },
+    {
+    "id": 6,
+    "modelo":"XIAOMI REDMI NOTE 12S Lte Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 307000,
+    "image":"./static/img/imagenes/6.png" ,
+    "tendencias": "OFERTA DEL DIA"
+    },
+    {
+    "id": 7,
+    "modelo":"XIAOMI REDMI NOTE 12 5G Dual SIM",
+    "ram": "4gb",
+    "almacenamiento": "128gb",
+    "precio": 247000,
+    "image":"./static/img/imagenes/7.png",
+    "tendencias":"24% OFF"
+    },
+    {
+    "id": 8,
+    "modelo":"XIAOMI REDMI NOTE 13 LTE Dual SIM",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 286000,
+    "image":"./static/img/imagenes/8.png",
+    "tendencias": "Llega mañana"
+    },
+    {
+    "id": 9,
+    "modelo":"XIAOMI REDMI NOTE 13 PRO LTE Dual SIM 4G",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 410000,
+    "image":"./static/img/imagenes/9.png" ,
+    "tendencias": "Llega gratis HOY"
+    },
+    {
+    "id": 10,
+    "modelo":"XIAOMI REDMI NOTE 13 PRO Dual SIM 5G",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 459000,
+    "image":"./static/img/imagenes/10.png",
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 11,
+    "modelo":"XIAOMI REDMI 13C Dual SIM",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 219000,
+    "image":"./static/img/imagenes/11.png",
+    "tendencias":"mismo precio en 3 cuotas de $ 73000"
+    },
+    {
+    "id": 12,
+    "modelo":"REALME 11 5G RMX3780 Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 369000,
+    "image":"./static/img/imagenes/12.png" ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 13,
+    "modelo":"REALME C55 RMX3710 Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 293000,
+    "image":"./static/img/imagenes/13.png"  ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 14,
+    "modelo":"SAMSUNG GALAXY A54 5G",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 500000,
+    "image":"./static/img/imagenes/14.png",
+    "tendencias": "10% OFF",
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 15,
+    "modelo":"SAMSUNG GALAXY A24",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 313000,
+    "image":"./static/img/imagenes/15.png"  ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 16,
+    "modelo":"SAMSUNG GALAXY A15 Lte Dual SIM",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 258000,
+    "image":"./static/img/imagenes/16.png"  ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 17,
+    "modelo":"SAMSUNG GALAXY A14 5G",
+    "ram": "4gb",
+    "almacenamiento": "128gb",
+    "precio": 258000,
+    "image":"./static/img/imagenes/17.png"  ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 18,
+    "modelo":"SAMSUNG GALAXY A05",
+    "ram": "4gb",
+    "almacenamiento": "128gb",
+    "precio": 196000,
+    "image":"./static/img/imagenes/18.png",
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 19,
+    "modelo":"MOTOROLA MOTO G54 Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 334000,
+    "image":"./static/img/imagenes/19.png",
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 20,
+    "modelo":"MOTOROLA MOTO G82 5G Dual SIM",
+    "ram": "6gb",
+    "almacenamiento": "128gb",
+    "precio": 300000,
+    "image":"./static/img/imagenes/20.png" ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 21,
+    "modelo":"MOTOROLA MOTO G52 Dual SIM",
+    "ram": "4gb",
+    "almacenamiento": "128gb",
+    "precio": 251000,
+    "image":"./static/img/imagenes/21.png" ,
+    "tendencias": "10% OFF"
+    },
+    {
+    "id": 22,
+    "modelo":"MOTOROLA MOTO G23 Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "128gb",
+    "precio": 219000,
+    "image":"./static/img/imagenes/22.png"  ,
+    "tendencias": "Llega gratis HOY"
+    },
+    {
+    "id": 23,
+    "modelo":"MOTOROLA MOTO G54 Dual SIM",
+    "ram": "8gb",
+    "almacenamiento": "256gb",
+    "precio": 450000,
+    "image":"./static/img/imagenes/23.png" ,
+    "tendencias": "Llega gratis HOY"
+    }
+
+]
+//array de memrorias RAM
+let memorias=[
+    {
+    "id":1,
+    "modelo":"MICRO SD SANDISK ULTRA 128GB",
+    "precio":12000,
+    "image":"./static/img/imagenes/27.png"
+    },
+    { 
+    "id":2,
+    "modelo" :"MICRO SDHC SANDISK ULTRA 32GB",
+    "precio" :6500,
+    "image":"./static/img/imagenes/28.png"
+    },
+    {
+    "id": 3,
+    "modelo":"MICRO SD SANDISK ULTRA 64GB",
+    "precio":8000,
+    "image":"./static/img/imagenes/29.png"
+    }
+
+]
+let parlantes=[
+    {
+    "id":1,
+    "modelo":"JBL GO 3 BLACK",
+    "precio":43000,
+    "image":"./static/img/imagenes/34.png"
+    },
+    { 
+    "id":2,
+    "modelo" :"SPEAKER WS-887",
+    "precio" :6000,
+    "image":"./static/img/imagenes/35.png"
+    },
+    {
+    "id": 3,
+    "modelo":"SPEAKER JBL Clip 4",
+    "precio": 55000,
+    "image":"./static/img/imagenes/36.png"
+    },
+    {
+    "id": 4,
+    "modelo":"ALTAVOZ JBL Flip 6",
+    "precio": 130000,
+    "image":"./static/img/imagenes/37.png"
+    },
+    {
+    "id": 5,
+    "modelo":"SPEAKER BOOMBASTIC SMART 100",
+    "precio": 35000,
+    "image":"./static/img/imagenes/38.png"
+    },
+    {
+    "id":6 ,
+    "modelo":"SPEAKER Y PROYECTOR DE LUZ PROSPER P-3111 ASTRONAUTA",
+    "precio": 18000,
+    "image":"./static/img/imagenes/39.png"
+    },
+    {
+    "id": 7,
+    "modelo":"MINI SPEAKER CAIXA DE SOM Portatil Luo LU-3166",
+    "precio": 20000,
+    "image":"./static/img/imagenes/40.png"
+    },
+    {
+    "id": 8,
+    "modelo":"SPEAKER PORTATIL BLULORY BS-J05",
+    "precio": 16000,
+    "image":"./static/img/imagenes/41.png"
+    },
+    {
+    "id": 9,
+    "modelo":"SPEAKER BLULORY BS-J02 CAIXA DE SOM",
+    "precio": 18000,
+    "image":"./static/img/imagenes/42.png"
+    },
+    {
+    "id": 10,
+    "modelo":"PORTATIL MISIK MS264 TWS",
+    "precio": 15000,
+    "image":"./static/img/imagenes/43.png"
+    },
+    {
+    "id": 11,
+    "modelo":"PORTATIL LUO LU-P3 MINI",
+    "precio": 10000,
+    "image":"./static/img/imagenes/44.png"
+    },
+    {
+    "id": 12,
+    "modelo":"SPEAKER SOONBOX S30",
+    "precio": 10000,
+    "image":"./static/img/imagenes/45.png"
+    },
+    {
+    "id": 13,
+    "modelo":"SPEAKER ECOPOWER EP-2521 VERMELHO",
+    "precio": 11000,
+    "image":"./static/img/imagenes/46.png"
+    },
+    {
+    "id": 14,
+    "modelo":"ALTAVOZ MINI CHARGE G2",
+    "precio": 5500,
+    "image":"./static/img/imagenes/47.png"
+    },
+    {
+    "id": 15,
+    "modelo":"AXTIVE MA MINI ALTAVOZ",
+    "precio": 15000,
+    "image":"./static/img/imagenes/48.png"
+    },
+    {
+    "id": 16,
+    "modelo":"ALTAVOZ MOVIL MULTIMEDIA MS-2233BT",
+    "precio": 7000,
+    "image":"./static/img/imagenes/49.png"
+    },
+    {
+    "id": 17,
+    "modelo":"CAJA DE SONIDO/ALTAVOZ MULTIMEDIA MÓVIL MS-1605BT",
+    "precio": 7000,
+    "image":"./static/img/imagenes/50.png"
+    },
+    {
+    "id": 18,
+    "modelo":"ALTAVOZ MOVIL MULTIMEDIA MS-2222BT",
+    "precio": 7000,
+    "image":"./static/img/imagenes/51.png"
+    },
+    {
+    "id": 19,
+    "modelo":"SPEAKER MOBILE LIGHT MODES MS-2234BT",
+    "precio": 7000,
+    "image":"./static/img/imagenes/52.png"
+    },
+    {
+    "id": 20,
+    "modelo":"PORTATIL CHARGE2+",
+    "precio": 10000,
+    "image":"./static/img/imagenes/53.png"
+    }
+
+]
+
+let variados=[
+    {
+    "id":1,
+    "modelo":"MEGASTAR LS408BT",
+    "precio":16000,
+    "image":"./static/img/imagenes/54.png"
+    },
+    { 
+    "id":2,
+    "modelo" :"CAJA DE ALTAVOZ LUMINARIA G con cargador de inducción magnética Altavoz",
+    "precio" :25000,
+    "image":"./static/img/imagenes/55.png"
+    },
+    {
+    "id":3,
+    "modelo":"LÁMPARA TÁCTIL CAJA DE SONIDO PORTÁTIL",
+    "precio":5500,
+    "image":"./static/img/imagenes/56.png"
+    }
+]
+let pendrives=[
+    {
+    "id":1,
+    "modelo":"SANDISK CRUZER BLADE 32GB Z50",
+    "precio":6000,
+    "image":"./static/img/imagenes/30.png"
+    },
+    { 
+    "id":2,
+    "modelo" :"SANDISK CRUZER BLADE 128GB Z50",
+    "precio" :12000,
+    "image":"./static/img/imagenes/31.png"
+    },
+    {
+    "id":3,
+    "modelo" :"SANDISK CRUZER BLADE 64GB Z50",
+    "precio" :7000,
+    "image":"./static/img/imagenes/32.png"
+    },
+    {
+    "id":4,
+    "modelo" :"SANDISK CRUZER BLADE 16GB Z50",
+    "precio" :5500,
+    "image":"./static/img/imagenes/33.png"
+    }
+]
+
+let cargadoresp=[
+    {
+    "id":1,
+    "modelo":"MAGNAVOX MAC6119",
+    "precio":20000,
+    "image":"./static/img/imagenes/24.png"
+    },
+    { 
+    "id":2,
+    "modelo" :"SATELLITE A-PB410002Q",
+    "precio" :15500,
+    "image":"./static/img/imagenes/25.png"
+    },
+    {
+    "id":3,
+    "modelo" :"SATELLITE A-PB410001",
+    "precio" :19000,
+    "image":"./static/img/imagenes/26.png"
+    }
+]
---
+++ static/JS/index.js
@@ -0,0 +1,174 @@
+// Se define una variable MiInicio como una cadena vacía (``). Esta variable se usará para almacenar el contenido HTML que se generará dinámicamente.
+let miInicio = ``
+
+//For para traer la información de data y mostrarla como tarjetas
+for(let elemento of celulares){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicio = miInicio +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>${elemento.tendencias}</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">precio: ${elemento.precio}</p>
+                                    <p class="movie-item-detail-subtitle">${elemento.ram}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+
+
+//Selecciono el main y con innerHTML agrego lo que definí en la variable miInicio
+document.querySelector('.productojs').innerHTML=miInicio
+
+let miInicioMemorias=``
+for(let elemento of memorias){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicioMemorias = miInicioMemorias +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>Las tendencias de hoy</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">${elemento.modelo}</p>
+                                    <p class="movie-item-detail-subtitle">precio: ${elemento.precio}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+document.querySelector('.memoriasjs').innerHTML=miInicioMemorias
+
+let miInicioParlantes=``
+for(let elemento of parlantes){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicioParlantes = miInicioParlantes +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>Las tendencias de hoy</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">${elemento.modelo}</p>
+                                    <p class="movie-item-detail-subtitle">precio: ${elemento.precio}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+document.querySelector('.parlantesjs').innerHTML=miInicioParlantes
+
+let miInicioVariados=``
+for(let elemento of variados){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicioVariados = miInicioVariados +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>Las tendencias de hoy</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">${elemento.modelo}</p>
+                                    <p class="movie-item-detail-subtitle">$${elemento.precio}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+document.querySelector('.variadosjs').innerHTML=miInicioVariados
+
+
+let miInicioPendrives=``
+for(let elemento of pendrives){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicioPendrives = miInicioPendrives +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>Las tendencias de hoy</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">${elemento.modelo}</p>
+                                    <p class="movie-item-detail-subtitle">precio: ${elemento.precio}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+document.querySelector('.pendrivesjs').innerHTML=miInicioPendrives
+
+let miInicioCargadoresp=``
+for(let elemento of cargadoresp){
+
+    //Voy iterando y agregando las tarjetas
+   
+    miInicioCargadoresp = miInicioCargadoresp +`
+                <!-- División contenedora del título e imágenes -->
+            <section id="popular">
+                <div class="container">
+                    <h3>Las tendencias de hoy</h3>
+                    <div id="list-popular">
+                        <div class="movie-item">
+                            <a href="./templates/detail-movie.html">
+                                <img src=${elemento.image}  alt="" class="movie-item-img">
+                                <div class="movie-item-detail"> 
+                                    <p class="movie-item-detail-title">${elemento.modelo}</p>
+                                    <p class="movie-item-detail-subtitle">precio: ${elemento.precio}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    
+    `
+}
+document.querySelector('.caradorPortatilesjs').innerHTML=miInicioCargadoresp
